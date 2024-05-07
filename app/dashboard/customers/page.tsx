c862d38b3afa4bd49e437046d87449af8010b457
zoujia <zoujia867@163.com>
2024-05-07 16:11:39 +0800

    使用RouteGroups功能组织页面文件演示skeleton效果

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -4,8 +4,9 @@ export default async function Page() {
     // 由于 loading.tsx 是在 dashboard 根目录的,所以其效果会自动应用到其子目录/路径,
     // 即 dashboard/customers, dashboard/invoices 2个页面,
     // 但是如果实际点击左侧导航跳转到相关页面,看不出加载的效果,是因为该 2 个页面没有实际数据,加载太快看不到,
-    // 所以,可以添加此行代码，以延迟 2 秒钟; invoices/page.tsx 页面也是类似
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    // 所以,可以添加此行代码，以延迟 2 秒钟; invoices/page.tsx 页面也是类似;
+    // 当使用 RouteGroups 之后,可以去掉此代码
+    // await new Promise(resolve => setTimeout(resolve, 2000));
 
     return <p>Customers Page</p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,6 +1,6 @@
 
 export default async function Page() {
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    // await new Promise(resolve => setTimeout(resolve, 2000));
 
     return <p>Invoices Page</p>;
 }
